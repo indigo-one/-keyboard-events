0985636dfc4d9f96fbe3eb61f2eb7f0cd6f8bdd5
indigo-one <54807953+indigo-one@users.noreply.github.com>
2020-02-09 22:52:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//"use strict";
+
+/*
+document.querySelector('#test').onkeypress = function (e) {
+ console.log(e.keyCode);
+ console.log(e.shiftKey);
+ if (e.keyCode < 48 || e.keyCode > 57) {
+   console.log('Недопустимые символы');
+ }
+ else {
+   this.value += e.key;
+ }
+
+
+
+ e.altKey false
+ e.charCode 97
+ e.code "KeyA"
+ e.ctrlKey false
+ e.key: "a"
+ e.keyCode: 97
+ e.shiftKey: false
+ return false; 
+
+}
+*/
+ 
+let left = 0;
+
+document.onkeypress = function(e) {
+  console.log(e.keyCode);
+  let sq = document.querySelector(".sq");
+  if (e.keyCode == 100) {
+    left = left + 10;
+    sq.style.marginLeft = left + "px";
+  }
+  if (e.keyCode == 97) {
+    left = left - 10;
+    sq.style.marginLeft = left + "px";
+  }
+};
+
+/*
+document.onkeypress = function () {
+  console.log('keypress');
+}
+document.onkeydown = function () {
+  console.log('keydown');
+}
+document.onkeyup = function () {
+  console.log('keyup');
+}
+*/
